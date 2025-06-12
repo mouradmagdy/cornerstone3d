1bcef6be96718294a392da55ce27f42069bc3ab7
Mourad Magdy <murad.ibrahim02@eng-st.cu.edu.eg>
2025-06-12 20:59:26 +0300

    init tailwindcss and shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import CornerstoneViewer from "./CornerstoneViewer";
 
 function App() {
-  // Example instance data (replace with actual DICOM metadata)
-  const instance = {
-    studyId: "1.2.3",
-    seriesId: "4.5.6",
-    cuadrId: "7.8.9",
-    frames: 1,
-    modality: "CT",
-  };
-
   return (
     <div>
-      <h1>Cornerstone3D React 18 Demo</h1>
-      <CornerstoneViewer instance={instance} />
+      <h1 className="bg-red-300">Cornerstone3D React 18 Demo</h1>
+      <CornerstoneViewer />
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { viteCommonjs } from "@originjs/vite-plugin-commonjs";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
+
     // for dicom-parser
     viteCommonjs(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   // seems like only required in dev mode
   optimizeDeps: {
     exclude: ["@cornerstonejs/dicom-image-loader"],
